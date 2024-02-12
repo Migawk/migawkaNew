e9690d10c19622f79c3b6c2a548096ec33d01ad9
migawk <yevhen.shevchen@gmail.com>
2024-02-12 06:43:55 +0100

    last edits. see README

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Portfolio
 
 [ ] Make portfolio
+
+
+## Chrome
+
+Idk what happens with Chrome, but I've been forced to write own version for chrome.
+
+Chrome is a bit mystery.
+
+Chrome has:
+    [ ] - another code structure
+    [ ] - another styles
--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,12 @@ import TheFooter from './modules/TheFooter.vue';
 let oldScrollY = window.scrollY;
 let ind = 0;
 let isScroll = false;
+let isChromeLike = false;
 
-window.onscroll = () => {
+if (navigator.userAgent.indexOf('Chrome') !== -1) isChromeLike = true;
+setTimeout(() => console.log(isChromeLike), 100);
+
+document.addEventListener('wheel', (e) => {
   const hello = document.getElementById('hello');
   const whyMe = document.getElementById('whyMe')! as HTMLElement;
   const portfolio = document.getElementById('portfolio');
@@ -24,163 +28,206 @@ window.onscroll = () => {
   const basement = document.querySelector('#basement')! as HTMLElement;
   const footer = document.querySelector('#footer')! as HTMLElement;
   const slides = [hello, whyMe, portfolio, skills, null, intrested, basement, footer];
+  console.log(e.deltaY);
+});
+// window.onscroll = () => {
+//   const hello = document.getElementById('hello');
+//   const whyMe = document.getElementById('whyMe')! as HTMLElement;
+//   const portfolio = document.getElementById('portfolio');
+//   const skills = document.getElementById('skillsSlide');
+//   const header = document.querySelector('header')! as HTMLElement;
+//   const headersBottomWave = document.querySelector('#headersBottomWave')! as HTMLElement;
+//   const intrested = document.querySelector('#intrested')! as HTMLElement;
+//   const basement = document.querySelector('#basement')! as HTMLElement;
+//   const footer = document.querySelector('#footer')! as HTMLElement;
+//   const slides = [hello, whyMe, portfolio, skills, null, intrested, basement, footer];
 
-  if (isScroll) {
-    if (ind === 1 || ind === 2 || ind === 4) return;
-    const scrollTo = Math.abs(
-      Math.round(window.scrollY + (slides[ind] as HTMLElement).getBoundingClientRect().top)
-    );
-    slides[ind] ? window.scrollTo(0, scrollTo) : null;
-    return;
-  }
-  isScroll = true;
-  setTimeout(() => {
-    isScroll = false;
-    oldScrollY = window.scrollY;
-  }, 1000);
-  
-  const blogSlide = document.querySelector('#blogSlide')! as HTMLElement;
-  const [bW1, bW2] = [document.querySelector('#blogWave1')!, document.querySelector('#blogWave2')!];
-  
-  const newScroll = window.scrollY;
-  const direction = oldScrollY < newScroll ? 'down' : 'up';
-  oldScrollY = newScroll;
+//   if (isChromeLike) {
+//     if(isScroll) return;
+//     isScroll = true;
 
-  if ((ind === 1 || ind === 2) && direction === 'down') {
-    if (slides[ind]!.getBoundingClientRect().y > 0) return;
-    const scrolled =
-      slides[ind]!.getBoundingClientRect().height + slides[ind]!.getBoundingClientRect().y;
+//     setTimeout(() => {
+//       isScroll = false;
+//       oldScrollY = window.scrollY;
+//     }, 1000);
 
-    if (window.innerHeight < scrolled) return;
-  }
-  
-  direction === 'down' ? (ind += 1) : (ind -= 1);
+//     const newScroll = window.scrollY;
+//     const direction = oldScrollY < newScroll ? 'down' : 'up';
 
-  console.log(ind, direction);
+//     oldScrollY = newScroll;
 
-  if (ind === 3 && direction === 'up') {
-    header.animate(
-      {
-        transform: 'translate(0px)'
-      },
-      {
-        duration: 250,
-        fill: 'forwards'
-      }
-    );
-    headersBottomWave.animate(
-      {
-        transform: 'rotate(180deg) translateY(6px)'
-      },
-      {
-        duration: 250,
-        fill: 'forwards'
-      }
-    );
-    blogSlide.animate(
-      {
-        transform: 'translate(0, 0)'
-      },
-      {
-        duration: 1000,
-        fill: 'forwards'
-      }
-    );
-    bW1.animate(
-      {
-        transform: 'translate(0px)'
-      },
-      {
-        duration: 4000,
-        fill: 'forwards'
-      }
-    );
-    bW2.animate(
-      {
-        transform: 'translate(0px)'
-      },
-      {
-        duration: 1000,
-        fill: 'forwards'
-      }
-    );
-  }
-  if (ind === 3 && direction === 'down') {
-    window.scrollTo(0, window.scrollY + (skills as HTMLElement).getBoundingClientRect().top);
-    return;
-  }
-  if (ind === 4 && direction !== 'down') {
-    window.scrollTo(0, window.scrollY + (skills as HTMLElement).getBoundingClientRect().top);
-    return;
-  }
-  if (ind === 4 && direction === 'down') {
-    window.scrollTo(0, window.scrollY + (skills as HTMLElement).getBoundingClientRect().top);
-    header.animate(
-      {
-        transform: 'translateY(-128px)'
-      },
-      {
-        duration: 250,
-        fill: 'forwards'
-      }
-    );
-    headersBottomWave.animate(
-      {
-        transform: 'rotate(180deg) translateY(128px)'
-      },
-      {
-        duration: 250,
-        fill: 'forwards'
-      }
-    );
-    blogSlide.animate(
-      {
-        transform: 'translate(calc(-100vw - 512px), 0)'
-      },
-      {
-        duration: 1000,
-        fill: 'forwards'
-      }
-    );
-    bW1.animate(
-      {
-        transform: 'translate(-128px)'
-      },
-      {
-        duration: 1000,
-        fill: 'forwards',
-        easing: 'cubic-bezier(1,.25,1,.25)'
-      }
-    );
-    bW2.animate(
-      {
-        transform: 'translate(-48px)'
-      },
-      {
-        duration: 1000,
-        fill: 'forwards'
-      }
-    );
-    return;
-  }
+//     if(direction === 'down') {
+//       ind++;
+//     } else {
+//       ind--;
+//     }
+//     if (ind < 0) ind = 0;
+//     if (ind > 7) ind = 7;
 
-  if (ind < 0) ind = 0;
-  if (ind > 7) ind = 7;
+//     console.log(ind, slides[ind]);
 
-  if (ind === 4 && direction === 'up') {
-    window.scrollTo(0, window.scrollY - window.innerHeight);
-    return;
-  }
-  const scrollTo = Math.abs(
-    Math.round(window.scrollY + (slides[ind] as HTMLElement).getBoundingClientRect().top)
-  );
+//     return;
+//   } else {
+//     if (isScroll) {
+//       if (ind === 1 || ind === 2 || ind === 4) return;
+//       const scrollTo = Math.abs(
+//         Math.round(window.scrollY + (slides[ind] as HTMLElement).getBoundingClientRect().top)
+//       );
+//       slides[ind] ? window.scrollTo(0, scrollTo) : null;
+//       return;
+//     }
+//     isScroll = true;
+//     setTimeout(() => {
+//       isScroll = false;
+//       oldScrollY = window.scrollY;
+//     }, 1000);
 
-  slides[ind]
-    ? setTimeout(() => {
-        window.scrollTo(0, scrollTo);
-      }, 1)
-    : null;
-};
+//     const blogSlide = document.querySelector('#blogSlide')! as HTMLElement;
+//     const [bW1, bW2] = [
+//       document.querySelector('#blogWave1')!,
+//       document.querySelector('#blogWave2')!
+//     ];
+
+//     const newScroll = window.scrollY;
+//     const direction = oldScrollY < newScroll ? 'down' : 'up';
+//     oldScrollY = newScroll;
+
+//     if ((ind === 1 || ind === 2) && direction === 'down') {
+//       if (slides[ind]!.getBoundingClientRect().y > 0) return;
+//       const scrolled =
+//         slides[ind]!.getBoundingClientRect().height + slides[ind]!.getBoundingClientRect().y;
+
+//       if (window.innerHeight < scrolled) return;
+//     }
+
+//     direction === 'down' ? (ind += 1) : (ind -= 1);
+
+//     console.log(ind, direction);
+
+//     if (ind === 3 && direction === 'up') {
+//       header.animate(
+//         {
+//           transform: 'translate(0px)'
+//         },
+//         {
+//           duration: 250,
+//           fill: 'forwards'
+//         }
+//       );
+//       headersBottomWave.animate(
+//         {
+//           transform: 'rotate(180deg) translateY(6px)'
+//         },
+//         {
+//           duration: 250,
+//           fill: 'forwards'
+//         }
+//       );
+//       blogSlide.animate(
+//         {
+//           transform: 'translate(0, 0)'
+//         },
+//         {
+//           duration: 1000,
+//           fill: 'forwards'
+//         }
+//       );
+//       bW1.animate(
+//         {
+//           transform: 'translate(0px)'
+//         },
+//         {
+//           duration: 4000,
+//           fill: 'forwards'
+//         }
+//       );
+//       bW2.animate(
+//         {
+//           transform: 'translate(0px)'
+//         },
+//         {
+//           duration: 1000,
+//           fill: 'forwards'
+//         }
+//       );
+//     }
+//     if (ind === 3 && direction === 'down') {
+//       window.scrollTo(0, window.scrollY + (skills as HTMLElement).getBoundingClientRect().top);
+//       return;
+//     }
+//     if (ind === 4 && direction !== 'down') {
+//       window.scrollTo(0, window.scrollY + (skills as HTMLElement).getBoundingClientRect().top);
+//       return;
+//     }
+//     if (ind === 4 && direction === 'down') {
+//       window.scrollTo(0, window.scrollY + (skills as HTMLElement).getBoundingClientRect().top);
+//       header.animate(
+//         {
+//           transform: 'translateY(-128px)'
+//         },
+//         {
+//           duration: 250,
+//           fill: 'forwards'
+//         }
+//       );
+//       headersBottomWave.animate(
+//         {
+//           transform: 'rotate(180deg) translateY(128px)'
+//         },
+//         {
+//           duration: 250,
+//           fill: 'forwards'
+//         }
+//       );
+//       blogSlide.animate(
+//         {
+//           transform: 'translate(calc(-100vw - 512px), 0)'
+//         },
+//         {
+//           duration: 1000,
+//           fill: 'forwards'
+//         }
+//       );
+//       bW1.animate(
+//         {
+//           transform: 'translate(-128px)'
+//         },
+//         {
+//           duration: 1000,
+//           fill: 'forwards',
+//           easing: 'cubic-bezier(1,.25,1,.25)'
+//         }
+//       );
+//       bW2.animate(
+//         {
+//           transform: 'translate(-48px)'
+//         },
+//         {
+//           duration: 1000,
+//           fill: 'forwards'
+//         }
+//       );
+//       return;
+//     }
+
+//     if (ind < 0) ind = 0;
+//     if (ind > 7) ind = 7;
+
+//     if (ind === 4 && direction === 'up') {
+//       window.scrollTo(0, window.scrollY - window.innerHeight);
+//       return;
+//     }
+//     const scrollTo = Math.abs(
+//       Math.round(window.scrollY + (slides[ind] as HTMLElement).getBoundingClientRect().top)
+//     );
+
+//     slides[ind]
+//       ? setTimeout(() => {
+//           window.scrollTo(0, scrollTo);
+//         }, 1)
+//       : null;
+//   }
+// };
 </script>
 
 <template>
@@ -199,7 +246,7 @@ window.onscroll = () => {
   </main>
 </template>
 
-<style scoped lang="sass">
+<style scoped lang="sass" v-if="!isChromeLike">
 main
   max-width: 100vw
   min-height: 100vh
@@ -217,4 +264,14 @@ the-page-slider
 #blogSlide
   height: 100vh
   min-width: 100vw // there must be position sticky
+</style>
+<style lang="sass" v-if="isChromeLike">
+main
+  width: 100%
+  max-height: 100%
+  overflow: hidden
+  position: absolute
+#whyMe,
+#portfolio
+  position: absolute
 </style>
